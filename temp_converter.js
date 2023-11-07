f6505d856490feb79cf33f0c61144093b81efbb4
Daniel Simmons <danieltsvs@gmail.com>
2023-11-07 11:26:39 -0600

    can convert fahrenheit to celsius

--- temp_converter.js
+++ temp_converter.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+function convertToCelsius(fahrenheit){
+    const celsius = (fahrenheit - 32) * (5/9);
+    return celsius;
+}
+
+function createMessage(fahrenheit, celsius){
+    return `The temperature is ${fahrenheit} degrees Fahrenheit. This is ${celsius} degrees Celsius.`;
+}
+
+const inputFahrenheit = window.prompt('What is the temperature in Fahrenheit?');
+
+const convertedCelsius = convertToCelsius(inputFahrenheit);
+
+console.log(createMessage(inputFahrenheit, convertedCelsius));
